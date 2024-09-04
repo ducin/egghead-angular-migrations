13be4b6dbc424f30c0577f916ac7a59735acbb60
Tomasz Ducin <tomasz@ducin.dev>
2024-09-04 22:50:12 +0200

    lesson 1

--- src/app/benefits/benefit-listing/benefit-listing.component.ts
+++ src/app/benefits/benefit-listing/benefit-listing.component.ts
@@ -1,31 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { BenefitsService } from 'src/app/api/benefits.service';
+import { Component, OnInit } from "@angular/core";
+import { BenefitsService } from "src/app/api/benefits.service";
 
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
-import { Benefit } from 'src/app/api/dto';
-import { BenefitsModule } from '../benefits.module';
-import { CommonModule } from '@angular/common';
+import { Benefit } from "src/app/api/dto";
+import { BenefitsModule } from "../benefits.module";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
   standalone: true,
-  imports: [
-      BenefitsModule,
-      CommonModule
-  ],
-  selector: 'itcorpo-benefit-listing',
-  templateUrl: './benefit-listing.component.html',
-  styleUrls: ['./benefit-listing.component.css']
+  imports: [BenefitsModule, AsyncPipe],
+  selector: "itcorpo-benefit-listing",
+  templateUrl: "./benefit-listing.component.html",
+  styleUrls: ["./benefit-listing.component.css"],
 })
 export class BenefitListingComponent implements OnInit {
-  benefits$!: Observable<Benefit[]>
+  benefits$!: Observable<Benefit[]>;
 
-  constructor(
-    private benefitSvc: BenefitsService,
-  ) { }
+  constructor(private benefitSvc: BenefitsService) {}
 
   ngOnInit() {
-    this.benefits$ = this.benefitSvc.getAllBenefits()
+    this.benefits$ = this.benefitSvc.getAllBenefits();
   }
-
 }
